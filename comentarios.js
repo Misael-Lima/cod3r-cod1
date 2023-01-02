02815a660eeb3022e377def1e7b07aa8efa147b6
Misael-Lima <misael865.nic@gmail.com>
2023-01-02 20:39:08 -0300

    array e objeto

--- comentarios.js
+++ comentarios.js
@@ -6,9 +6,38 @@ console.log("linha");
  * multiplas linhas
  */
 
-console.log("linha2");          //comentario
+console.log("linha2"); //comentario
 
 /*
 comentario de multiplas linhas
 
 */
+
+
+
+
+
+//array e objeto
+
+{
+  let michel = [
+    "veloso",
+    2,
+    {
+      key: "valor",
+      kel: 17,
+    },
+  ];
+
+  //michel.pop();
+  console.log(michel);
+  michel.push(7);
+  console.log(michel);
+}
+
+let objeto = {
+  key: "valor",
+  kel: 17,
+};
+
+console.log(objeto.kel);
